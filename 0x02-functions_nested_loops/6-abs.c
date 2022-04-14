f9382eabc901eca931036f001b94d7765183a4cb
zoolaaa <mzele338@gmail.com>
2022-04-14 03:20:56 +0300

    nested loop

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,24 +1,15 @@
 #include "main.h"
 /**
-*print_sign-prints sign of a number
-*@n: character to be verified
-*Return: return 0 or 1
+* _abs - prints the absolute value of a number
+*@i: number targeted
+*Return: returns 0
 */
-int print_sign(int n)
+int _abs(int i)
 {
-if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else if (n > 0)
-{
-_putchar('+');
-return (1);
-}
+if (i > 0)
+return (i);
+else if (i < 0)
+return (-i);
 else
-{
-_putchar('-');
-return (-1);
-}
+return (0);
 }
